534adf7f4ea09c1c8ce8c43228ae252c5b0ea683
esiteq <bugrov@gmail.com>
2013-11-01 10:19:54 +0200

    updates by rolland

--- Bender/Bender.class.php
+++ Bender/Bender.class.php
@@ -5,7 +5,8 @@
  * @website http://www.esiteq.com/
  * @email bugrov at gmail.com
  * @created 29.10.2013
- * @version 1.0
+ * @version 0.2
+ * improved by Rolland (rolland at alterego.biz.ua)
  */
 
 class Bender
@@ -19,6 +20,7 @@ class Bender
     // Project's root dir
     public $root_dir;
     // Constructor
+    private $version_key = 'v';
     public function __construct()
     {
         $this->root_dir = defined( 'ROOT_DIR' ) ? ROOT_DIR : $_SERVER['DOCUMENT_ROOT'];
@@ -98,16 +100,19 @@ class Bender
     // Print output for CSS or Javascript
     public function output( $output )
     {
+        $output = ltrim( $output, './' );
         global $_javascripts, $_stylesheets;
         switch ( $this->get_ext( $output ) )
         {
             case "css":
+                $this->check_recombine( $output, $_stylesheets );
                 $this->minify( $_stylesheets, "css", $output );
-                return '<link href="/' . $output . '" rel="stylesheet" type="text/css"/>';
+                return '<link href="' . $this->get_src( $output ) . '" rel="stylesheet" type="text/css"/>';
                 break;
             case "js":
+                $this->check_recombine( $output, $_javascripts );
                 $this->minify( $_javascripts, "js", $output );
-                return '<script type="text/javascript" src="/' . $output . '"></script>';
+                return '<script type="text/javascript" src="' . $this->get_src( $output ) . '"></script>';
                 break;
         }
     }
@@ -136,5 +141,43 @@ class Bender
     {
         return strtolower( pathinfo( $src, PATHINFO_EXTENSION ) );
     }
+    /**
+     * Gheck if need to recombine output file
+     */
+    protected function check_recombine( $output, $files )
+    {
+        $path = $this->root_dir();
+        $outfile = "{$path}/{$output}";
+        if ( !file_exists( $outfile ) || !is_array( $files ) )
+        {
+            return;
+        }
+        // find last modify time of src
+        $last = 0;
+        foreach ( $files as $file )
+        {
+            if ( ( $_time = filemtime( $path . "/" . $file ) ) > $last )
+                $last = $_time;
+        }
+        if ( filemtime( $outfile ) < $last )
+        {
+            // need to be recombined
+            $this->ttl = 0;
+        }
+        else
+        {
+            $this->ttl = -1;
+        }
+    }
+
+    /**
+     * returns src for resource due to filemtime
+     */
+    protected function get_src( $output )
+    {
+        $path = $this->root_dir();
+        return '/' . $output . '?' . $this->version_key . '=' . filemtime( $path . "/" . $output );
+    }
+
 }
 ?>
